8bad4a03d8acb5156e77a044b5b7c8fc309555b8
RobertWoodhouse <RMDWoodhouse@live.com>
2019-07-19 06:36:58 +0100

    New Scripts and Graphics

---
+++ Rubik's Riddim/Assets/Scripts/AudioController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour 
+{
+	public void PlayAudio(AudioClip clip, AudioSource source)
+	{
+		source.clip = clip;
+		source.Play();
+	}
+
+    public void ThemeTrasition()
+	{
+		
+	}
+}
+
+
