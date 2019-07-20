5649823e206411fcfe176d8233c92e5878abb5ac
RobertWoodhouse <RMDWoodhouse@live.com>
2019-07-20 21:29:34 +0100

    Created Enemy GameObject with Child Pieces

---
+++ Rubik's Riddim/Assets/Scripts/EnemyMovementController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IEnemyMovementController
+{	
+}
+
+public class EnemyMovementController : IEnemyMovementController 
+{
+	public void BasicDown()
+    {
+//        _enemySpeedX = Random.Range(enemySpeedXMin, enemySpeedXMax);
+
+  //      _enemyRB.AddForce(-transform.up * _enemySpeedX); // Vertical Down Speed
+    }
+}
