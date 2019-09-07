2b3dfa5d2d82d226d2542ac0a758c084140be7de
RobertWoodhouse <RMDWoodhouse@live.com>
2019-09-07 10:45:43 +0100

    Corrected Collision detection

--- Rubik's Riddim/Assets/Scripts/TouchController.cs
+++ Rubik's Riddim/Assets/Scripts/TouchController.cs
@@ -4,10 +4,39 @@ public class TouchController : MonoBehaviour
 { 
 	private void Update()
 	{
-    	Touchpad();    
-		TouchScreen(); // TODO: fix ArgumentException
-	}
+    	//Touchpad();    
+		//TouchScreen(); // TODO: fix ArgumentException
 
+		#if UNITY_EDITOR
+		if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero);
+
+            if (hit != false && hit.collider != null)
+            {
+                //Debug.Log("Currently touching gameObject: " + hit.collider.name + " | direction: " + hit.collider.tag);
+                GameEvents.S.TouchpadDown(hit.collider.GetComponentInParent<PieceController>().Id, hit.collider.tag); // Pass ID from PieceController script
+            }
+        }
+		#endif
+
+		#if UNITY_EDITOR
+		if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            Vector3 pos = Camera.main.ScreenToWorldPoint(touch.position);
+            RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero);
+
+            if (hit != false && hit.collider != null)
+            {
+                Debug.Log("Currently touching gameObject: " + hit.collider.name + " | direction: " + hit.collider.tag);
+                GameEvents.S.TouchpadDown(hit.collider.GetComponentInParent<PieceController>().Id, hit.collider.tag); // Pass ID from PieceController script
+            }
+        }
+		#endif
+	}
+    /*
 	#if UNITY_EDITOR
 	private void Touchpad()
 	{
@@ -42,4 +71,5 @@ public class TouchController : MonoBehaviour
 		}
     }
 	#endif
+	*/
 }
