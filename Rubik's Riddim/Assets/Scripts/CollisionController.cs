b9ed2c95ee8c23f967a7e78deba2ccad5834cad2
RobertWoodhouse <RMDWoodhouse@live.com>
2019-09-22 10:01:01 +0100

    Updated controls

--- Rubik's Riddim/Assets/Scripts/CollisionController.cs
+++ Rubik's Riddim/Assets/Scripts/CollisionController.cs
@@ -4,6 +4,6 @@ public class CollisionController : MonoBehaviour
 {
 	private void OnTriggerEnter2D(Collider2D coll)
     {
-		StartCoroutine(gameObject.GetComponentInParent<PieceController>().OnPieceReturn(coll.gameObject));
+		  StartCoroutine(gameObject.GetComponentInParent<PieceController>().OnPieceReturn(coll.gameObject));
     }
 }
--- Rubik's Riddim/Assets/Scripts/TouchController.cs
+++ Rubik's Riddim/Assets/Scripts/TouchController.cs
@@ -4,9 +4,6 @@ public class TouchController : MonoBehaviour
 { 
 	private void Update()
 	{
-    	//Touchpad();    
-		//TouchScreen(); // TODO: fix ArgumentException
-
 		#if UNITY_EDITOR
 		if (Input.GetMouseButtonDown(0))
         {
@@ -21,7 +18,7 @@ public class TouchController : MonoBehaviour
         }
 		#endif
 
-		#if UNITY_EDITOR
+		#if UNITY_ANDROID
 		if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
@@ -30,46 +27,10 @@ public class TouchController : MonoBehaviour
 
             if (hit != false && hit.collider != null)
             {
-                Debug.Log("Currently touching gameObject: " + hit.collider.name + " | direction: " + hit.collider.tag);
+                //Debug.Log("Currently touching gameObject: " + hit.collider.name + " | direction: " + hit.collider.tag);
                 GameEvents.S.TouchpadDown(hit.collider.GetComponentInParent<PieceController>().Id, hit.collider.tag); // Pass ID from PieceController script
             }
         }
 		#endif
 	}
-    /*
-	#if UNITY_EDITOR
-	private void Touchpad()
-	{
-		if (Input.GetMouseButtonDown(0))
-		{
-			Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-			RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero);
-
-			if (hit != false && hit.collider != null)
-			{
-				//Debug.Log("Currently touching gameObject: " + hit.collider.name + " | direction: " + hit.collider.tag);
-				GameEvents.S.TouchpadDown(hit.collider.GetComponentInParent<PieceController>().Id, hit.collider.tag); // Pass ID from PieceController script
-			}
-		}
-	}
-	#endif
-
-	#if UNITY_ANDROID
-	private void TouchScreen()
-    {
-		if (Input.touchCount > 0)
-		{
-			Touch touch = Input.GetTouch(0);
-            Vector3 pos = Camera.main.ScreenToWorldPoint(touch.position);
-            RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero);
-
-            if (hit != false && hit.collider != null)
-            {
-                Debug.Log("Currently touching gameObject: " + hit.collider.name + " | direction: " + hit.collider.tag);
-                GameEvents.S.TouchpadDown(hit.collider.GetComponentInParent<PieceController>().Id, hit.collider.tag); // Pass ID from PieceController script
-            }
-		}
-    }
-	#endif
-	*/
 }
